77a3cff56833ad7a72705694a2c48901d4cf3f61
Simone Basso <bassosimone@gmail.com>
2018-07-04 12:16:08 +0200

    Allow to override example
    With this functionality, the PoC is probably complete.

--- cmd/libndt-go-client/main.go
+++ cmd/libndt-go-client/main.go
@@ -6,12 +6,13 @@ package main
 
 import (
 	"github.com/measurement-kit/libndt-go/libndt"
+	"log"
 	"os/signal"
 	"syscall"
 )
 
 func main() {
-	// TODO(bassosimone): correctly deal with signals is libndt
+	// TODO(bassosimone): correctly deal with signals in libndt
 	signal.Ignore(syscall.SIGPIPE)
 	settings := libndt.NewSettings()
 	settings.NettestFlags |= libndt.NettestFlagDownload | libndt.NettestFlagUpload
@@ -19,6 +20,24 @@ func main() {
 	settings.ProtocolFlags |= libndt.ProtocolFlagJSON
 	settings.Metadata["client.application"] = "measurement-kit/libndt-go"
 	settings.MlabnsURL = "https://mlab-ns.appspot.com/ndt?policy=random"
+	settings.OnWarningCallbacks = append(settings.OnWarningCallbacks, func(s string) {
+		log.Printf("<warn> %s\n", s)
+	})
+	settings.OnInfoCallbacks = append(settings.OnInfoCallbacks, func(s string) {
+		log.Printf("<info> %s\n", s)
+	})
+	settings.OnDebugCallbacks = append(settings.OnDebugCallbacks, func(s string) {
+		log.Printf("<debug> %s\n", s)
+	})
+	settings.OnPerformanceCallbacks = append(settings.OnPerformanceCallbacks, func(dir libndt.NettestFlags, nconn byte, numBytes float64, interval float64, elapsed float64, maxRuntime float64) {
+		log.Printf("<performance> %d - %d flows - %f kbit/s - %f completion\n", uint8(dir), nconn, numBytes*8.0/interval/1000.0, elapsed/maxRuntime)
+	})
+	settings.OnResultCallbacks = append(settings.OnResultCallbacks, func(scope string, name string, value string) {
+		log.Printf("<result> %s.%s = %s\n", scope, name, value)
+	})
+	settings.OnServerBusyCallbacks = append(settings.OnServerBusyCallbacks, func(m string) {
+		log.Printf("<server-busy> %s\n", m)
+	})
 	client := libndt.NewClientWithSettings(settings)
 	client.Run()
 }
--- libndt/libndt.go
+++ libndt/libndt.go
@@ -6,7 +6,6 @@ package libndt
 
 import (
 	"github.com/measurement-kit/libndt-go/swig"
-	"log"
 )
 
 // Version contains a version number
@@ -63,29 +62,62 @@ var ProtocolFlagWebSockets = ProtocolFlags(swig.GetProtocolFlagWebsockets())
 // Timeout is a timeout in seconds
 type Timeout = uint
 
+// LogCallback is a callback called when we receive log messages
+type LogCallback = func(string)
+
+// TODO(bassosimone): use a data structure for the performance callback as that
+// is more easily understandable and useable.
+
+// PerformanceCallback is a callback called when we receive performance info. The
+// arguments are: 1) NettestFlags containing either NettestFlagDownload or
+// NettestFlagUpload; 2) byte containing the number of parallel flows; 3) The
+// number of bytes received or sent during the measurement period; 4) The
+// duration of the measurement interval; 5) The elapsed time since this subtest
+// started; 6) The maximum runtime for this subtest.
+type PerformanceCallback = func(NettestFlags, byte, float64, float64, float64, float64)
+
+// ResultCallback is a callback called to provide you back results. The first
+// argument is the scope, the second is the variable name, the third its value.
+type ResultCallback = func(string, string, string)
+
+// ServerBusyCallback is called when the server is busy.
+type ServerBusyCallback = func(string)
+
 // Settings contains NDT client settings. When a field is not set, the default
-// value configured insider of libndt will be used.
+// value configured inside of libndt will be used.
 type Settings struct {
 	// MlabnsURL contains the mlab-ns URL to use.
-	MlabnsURL     string
+	MlabnsURL string
 	// Timeout indicates the timeout for I/O, in seconds.
-	Timeout       Timeout
+	Timeout Timeout
 	// Hostname is the host to connect to.
-	Hostname      string
+	Hostname string
 	// Port is the port to use.
-	Port          string
+	Port string
 	// Metadata contains additional metadata to be sent to the server.
-	Metadata      map[string]string
+	Metadata map[string]string
 	// NettestFlags contains flags selecting specific sub tests.
-	NettestFlags  NettestFlags
+	NettestFlags NettestFlags
 	// Verbosity controls the test verbosity.
-	Verbosity     Verbosity
+	Verbosity Verbosity
 	// ProtocolFlags selects specific protocl features.
 	ProtocolFlags ProtocolFlags
 	// MaxRuntime is the maximum amount of seconds for which a subtest can run.
-	MaxRuntime    Timeout
+	MaxRuntime Timeout
 	// SOCKS5hPort is the port to eventually be used for SOCKSv5.
-	SOCKS5hPort   string
+	SOCKS5hPort string
+	// OnWarningCallbacks is a list of callbacks called on WARNING messages.
+	OnWarningCallbacks []LogCallback
+	// OnInfoCallbacks is a list of callbacks called on INFO messages.
+	OnInfoCallbacks []LogCallback
+	// OnDebugCallbacks is a list of callbacks called on DEBUG messages.
+	OnDebugCallbacks []LogCallback
+	// OnPerformanceCallbacks is a list of callbacks called to handle PERFORMANCE.
+	OnPerformanceCallbacks []PerformanceCallback
+	// OnResultCallbacks is a list of callbacks called to handle RESULT.
+	OnResultCallbacks []ResultCallback
+	// OnServerBusyCallbacks is a list of callbacks called when the server is busy.
+	OnServerBusyCallbacks []ServerBusyCallback
 }
 
 // NewSettings creates an instance of settings
@@ -112,7 +144,7 @@ func NewClientWithSettings(s Settings) Client {
 
 // golangClient implements the swig.NdtClient interface.
 type golangClient struct {
-	// TODO(bassosimone): add here room for the callbacks to override
+	Settings *Settings
 }
 
 // Swigcptr() is part of the swig.NdtClient interface.
@@ -135,38 +167,50 @@ func (*golangClient) Run() bool {
 }
 
 // OnWarning() is part of the swig.NdtClient interface.
-func (*golangClient) OnWarning(msg string) {
-	log.Printf("WARNING: %s\n", msg)
+func (c *golangClient) OnWarning(msg string) {
+	for i := 0; i < len(c.Settings.OnWarningCallbacks); i += 1 {
+		c.Settings.OnWarningCallbacks[i](msg)
+	}
 }
 
 // OnInfo() is part of the swig.NdtClient interface.
-func (*golangClient) OnInfo(msg string) {
-	log.Printf("INFO: %s\n", msg)
+func (c *golangClient) OnInfo(msg string) {
+	for i := 0; i < len(c.Settings.OnInfoCallbacks); i += 1 {
+		c.Settings.OnInfoCallbacks[i](msg)
+	}
 }
 
 // OnDebug() is part of the swig.NdtClient interface.
-func (*golangClient) OnDebug(msg string) {
-	log.Printf("DEBUG: %s\n", msg)
+func (c *golangClient) OnDebug(msg string) {
+	for i := 0; i < len(c.Settings.OnDebugCallbacks); i += 1 {
+		c.Settings.OnDebugCallbacks[i](msg)
+	}
 }
 
 // OnPerformance() is part of the swig.NdtClient interface.
-func (*golangClient) OnPerformance(nettestID byte, numFlows byte, measuredBytes float64, measurementInterval float64, elapsed float64, maxRuntime float64) {
-	log.Printf("PERFORMANCE\n")
+func (c *golangClient) OnPerformance(nettestID byte, numFlows byte, measuredBytes float64, measurementInterval float64, elapsed float64, maxRuntime float64) {
+	for i := 0; i < len(c.Settings.OnPerformanceCallbacks); i += 1 {
+		c.Settings.OnPerformanceCallbacks[i](NettestFlags(nettestID), numFlows, measuredBytes, measurementInterval, elapsed, maxRuntime)
+	}
 }
 
 // OnResult() is part of the swig.NdtClient interface.
-func (*golangClient) OnResult(scope string, name string, value string) {
-	log.Printf("RESULT: %s.%s=%s\n", scope, name, value)
+func (c *golangClient) OnResult(scope string, name string, value string) {
+	for i := 0; i < len(c.Settings.OnResultCallbacks); i += 1 {
+		c.Settings.OnResultCallbacks[i](scope, name, value)
+	}
 }
 
 // OnServerBusy() is part of the swig.NdtClient interface.
-func (*golangClient) OnServerBusy(reason string) {
-	log.Printf("BUSY: %s\n", reason)
+func (c *golangClient) OnServerBusy(reason string) {
+	for i := 0; i < len(c.Settings.OnServerBusyCallbacks); i += 1 {
+		c.Settings.OnServerBusyCallbacks[i](reason)
+	}
 }
 
 // SwigIsGolangClient() is used to distinguish this type in the type system. See
 // also https://github.com/swig/swig/issues/418.
-func (*golangClient) SwigIsGolangClient() {
+func (c *golangClient) SwigIsGolangClient() {
 }
 
 // newDirectorNdtClient() provide type safety checks to swig.NewDirectoryNdtClient()
@@ -208,7 +252,7 @@ func (clnt Client) Run() bool {
 	// Here we pass the client with overriden methods as underlying
 	// implementation to the director class, such that the overriden
 	// methods of the Go class are called by the C++ code.
-	overriden := &golangClient{}
+	overriden := &golangClient{Settings: &clnt.settings}
 	director := newDirectorNdtClient(overriden, cxxSettings)
 	defer swig.DeleteDirectorNdtClient(director)
 	return director.Run()
